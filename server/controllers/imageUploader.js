74a1cd3bfffbf418e9ba2e044a21ae13693e73a3
Adarsh Singh <officialadarsh2021@gmail.com>
2024-05-14 22:00:26 +0530

    making cloud services working

--- server/controllers/imageUploader.js
+++ server/controllers/imageUploader.js
@@ -5,7 +5,7 @@ const AppError = require('./../utils/appError');
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-	cloud_name: process.env.CLOUD_NAME,
+	cloud_name: process.env.CLOUDINARY_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
