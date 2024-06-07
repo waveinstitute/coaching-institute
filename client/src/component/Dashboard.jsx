346910bf2bb8b2e117080998a9f8ff177f9f4ca8
Adarsh Singh <officialadarsh2021@gmail.com>
2024-06-07 17:00:40 +0530

    fix dist

--- client/src/component/Dashboard.jsx
+++ client/src/component/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
 	const { to } = useParams();
 	const { user, error } = useGetUser();
 	useTitle(`Dashboard`);
-
+	console.log(to);
 	return (
 		<div>
 			{error && <NotFound />}{' '}
